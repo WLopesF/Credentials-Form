6d677db786d2696579ec153bc61580c94eb94470
WLopesF <williamlopesfreitas789@gmail.com>
2024-11-22 20:12:39 -0300

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS user_data (
+    id INT PRIMARY KEY AUTOINCREMENT,  
+    full_name TEXT,
+    age INT,
+    email TEXT, 
+    fundamental_completed TEXT,
+    high_school_completed TEXT, 
+    skin_color TEXT,
+    gender TEXT,
+    preferred_course TEXT,
+    terms_agreed TEXT
+);
